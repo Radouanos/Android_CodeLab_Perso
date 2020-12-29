b36c74a94f0f0b09cd41aa8ca8e9c19327d9ab88
ELBISSIS Radouane <redwan.b6@gmail.com>
2020-12-29 19:05:00 +0100

    Initiate the repository and the database

---
+++ app/src/main/java/com/android/example/roomwordsample/WordsApplication.kt
@@ -0,0 +1,8 @@
+package com.android.example.roomwordsample
+
+import android.app.Application
+
+class WordsApplication :Application(){
+    val database by lazy { WordRoomDatabase.getDatabase(this) }
+    val repository by lazy { WordRepository(database.wordDao()) }
+}
