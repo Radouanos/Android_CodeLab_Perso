ea60ae99ae0f228f1eeaa729f1da468de54cab52
ELBISSIS Radouane <redwan.b6@gmail.com>
2020-12-29 19:40:03 +0100

    Populate Database

--- app/src/main/java/com/android/example/roomwordsample/Word.kt
+++ app/src/main/java/com/android/example/roomwordsample/Word.kt
@@ -5,4 +5,4 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName="word_table") //each entity represents an SQLITE table
-data class Word(@PrimaryKey(autoGenerate = true) val id:Int,@ColumnInfo(name = "word") val word:String )
+data class Word(@ColumnInfo(name = "word") val word:String )
